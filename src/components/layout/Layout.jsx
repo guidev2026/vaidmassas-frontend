a44bfac0a9f0d741ea53317c9fdb688eb8c3d9c7
guidev2026 <guidev2026@gmail.com>
2026-04-19 13:27:54 -0300

    Update Layout.jsx

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -5,7 +5,7 @@ export default function Layout() {
   return (
     <div className="flex min-h-screen bg-zinc-100">
       <Sidebar />
-      <main className="flex-1 p-8 overflow-y-auto">
+      <main className="flex-1 p-4 md:p-8 overflow-y-auto pt-20 md:pt-8">
         <Outlet />
       </main>
     </div>
